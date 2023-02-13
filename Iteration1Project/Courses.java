887028fdde771bba142e5a4c0660a9410c7d777e
Dave90910 <dave@localhost>
2023-02-12 23:22:25 -0500

    This program stores courses under satisfied/unsatisfied course list

---
+++ Iteration1Project/Courses.java
@@ -0,0 +1,32 @@
+package Iteration1Project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Courses {
+		
+		//initialize lists
+	    private List<String> satisfiedCourses = new ArrayList<>();
+	    private List<String> unsatisfiedCourses = new ArrayList<>();
+
+	    //adds the course to the corresponding lists depending on the grade
+	    public void storeCourseList(String courseName, int grade) {
+	        if (grade >= 50) {
+	            satisfiedCourses.add(courseName);
+	        } 
+	        else {
+	            unsatisfiedCourses.add(courseName);
+	        }
+	    }
+	    //getter method for unsatisfied courses
+	    public List<String> getUnsatisfiedCourses() {
+	        return unsatisfiedCourses;
+	    }
+	    //getter method for satisfied courses
+	    public List<String> getSatisfiedCourses() {
+	        return satisfiedCourses;
+	    }
+
+	   
+}
