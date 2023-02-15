84a34ef6178057f3766fa677e576f0604dab6d38
Dave90910 <dave@localhost>
2023-02-15 13:37:46 -0500

    Push Test

--- Iteration1Project/Student management system/src/Backend/Courses.java
+++ Iteration1Project/Student management system/src/Backend/Courses.java
@@ -11,12 +11,36 @@ public class Courses {
 	    private List<String> unsatisfiedCourses = new ArrayList<>();
 
 	    //adds the course to the corresponding lists depending on the grade
-	    public void storeCourseList(String courseName, int grade) {
-	        if (grade >= 50) {
-	            satisfiedCourses.add(courseName);
+	    
+	    
+	    public boolean validInput (String grade) {
+	    	
+	    	ArrayList<String> a = new ArrayList<String>();
+	    	
+	    	a.add("A+");
+	    	a.add("A");
+	    	a.add("B+");
+	    	a.add("B");
+	    	a.add("C+");
+	    	a.add("C");
+	    	a.add("D+");
+	    	a.add("D");
+	    	a.add("F");
+	    	a.add("E");
+	    	
+	    	
+	    	return a.contains(grade);
+	    }
+	    public void storeCourseList(String courseName, String grade) {
+	    	
+	    	if (!validInput(grade)) {
+	    		throw new IllegalArgumentException();
+	    	}
+	        if (grade == "E" || grade == "F") {
+	        	unsatisfiedCourses.add(courseName);
 	        } 
 	        else {
-	            unsatisfiedCourses.add(courseName);
+	            satisfiedCourses.add(courseName);
 	        }
 	    }
 	    //getter method for unsatisfied courses
