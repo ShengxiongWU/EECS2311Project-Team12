f5941340c407ced568c64a96c0568e5186bebfe5
Matthew <matthew2898@hotmail.com>
2023-02-14 21:58:04 -0500

    Change the query so the while loop isn't necessary

--- Iteration1Project/Student management system/src/Backend/DatabaseController.java
+++ Iteration1Project/Student management system/src/Backend/DatabaseController.java
@@ -6,39 +6,37 @@ public class DatabaseController {
 	// database connection details
 	private final String url = "jdbc:mysql://localhost:3306/report";
 	private final String user = "admin";
-	private final String pass = "!!MouseClicking!!";
+	private final String pass = "!!PrivateEyes!!";
 	private Connection con;
 
 	public DatabaseController() {
 		try {
 			con = DriverManager.getConnection(url,user,pass);
+			System.out.println("CONNECTED TO DB!");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public List<String> getStudentCourses(int studentID) {
 		/*
 		 * Args: the student ID you want the courses for.
 		 * Returns: List of the courses that they have enrolled in.
 		 */
-		String query ="SELECT student_id,course_id,status FROM course_enrollment";
+		String query ="SELECT student_id,course_id,status FROM course_enrollment WHERE student_id=" + studentID + " and status='Completed'";
 		List<String> courses = new ArrayList<>();
 		try(
 				Statement statement = con.createStatement();
 				ResultSet res = statement.executeQuery(query);
-			){
+				){
 			while(res.next()) {
-				
-				if(res.getInt(1) == studentID) {
-					courses.add(res.getString("course_id"));
-				}
+				courses.add(res.getString("course_id"));
 			}
 			return courses;
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return courses;
 	}
 
--- Iteration1Project/Student management system/src/module-info.java
+++ Iteration1Project/Student management system/src/module-info.java
@@ -2,4 +2,5 @@ module EECS2311Project {
 	requires java.desktop;
 	requires jdk.jpackage;
 	requires java.sql;
+	requires org.junit.jupiter.api;
 }
