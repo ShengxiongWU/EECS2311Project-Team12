830b5870d7715def169e8fc017f84ab5652acd47
Dave90910 <dave@localhost>
2023-03-18 10:56:19 -0400

    Comment

--- lib/src/main/java/Backend/Admin.java
+++ lib/src/main/java/Backend/Admin.java
@@ -6,16 +6,22 @@ public class Admin extends User{
 	private String Faculty;
 
 	
-	
+	/*
+     * Admin Constructor 
+     */
 	public Admin(String faculty,String ID, String Account, String Password) {
 		super(ID,Account,Password);
 		Faculty = faculty;
 	}
-
+    /*
+     * getter method for Faculty
+     */
 	public String getFaculty() {
 		return Faculty;
 	}
-
+	/*
+     * setter method for Faculty
+     */
 	public void setFaculty(String faculty) {
 		Faculty = faculty;
 	}
