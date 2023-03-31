8fe4e5893e9756c92388b6413659abafa85c2a12
Dave90910 <dave@localhost>
2023-03-31 16:17:24 -0400

    refactoring

--- lib/src/main/java/Backend/Course.java
+++ lib/src/main/java/Backend/Course.java
@@ -6,12 +6,14 @@ import DB.DB;
 
 //Course class
 public class Course {
+	//Initialize attributes for courses
 	private String course_id;
 	private String name;
 	private double credit;
 	private ArrayList<Course> prerequisites;
 	private DB db = DB.getInstance();
 	
+	//Course constructor 
 	public Course(String course_id,String name, double credit, ArrayList<Course> prerequisites) {
 		this.course_id = course_id;
 		this.credit = credit;
@@ -23,35 +25,51 @@ public class Course {
 		}
 		db.addCourse(course_id, name, String.valueOf(credit), pre);
 	}
-
+    /*
+     * getter method for courseID
+     */
 	public String getCourse_id() {
 		return course_id;
 	}
-
+	/*
+     * setter method for courseID
+     */
 	public void setCourse_id(String course_id) {
 		this.course_id = course_id;
 	}
-
+	/*
+     * getter method for course name
+     */
 	public String getName() {
 		return name;
 	}
-
+	/*
+     * setter method for course name
+     */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/*
+     * getter method for course credit
+     */
 	public double getCredit() {
 		return credit;
 	}
-
+	/*
+     * setter method for course credit
+     */
 	public void setCredit(double credit) {
 		this.credit = credit;
 	}
-
+	/*
+     * getter method for course prerequisites
+     */
 	public ArrayList<Course> getPrerequisites() {
 		return prerequisites;
 	}
-
+	/*
+     * setter method for course name
+     */
 	public void setPrerequisites(ArrayList<Course> prerequisites) {
 		this.prerequisites = prerequisites;
 	}
