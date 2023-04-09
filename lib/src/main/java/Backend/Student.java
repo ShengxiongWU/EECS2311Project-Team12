4d7913a1796d08facfde0d52d5a6424b368c5ea5
Dave90910 <dave@localhost>
2023-04-09 15:44:44 -0400

    Refactored-comment and variable name

--- lib/src/main/java/Backend/Student.java
+++ lib/src/main/java/Backend/Student.java
@@ -3,42 +3,57 @@ package Backend;
 import DB.DB;
 
 public class Student extends User{
-	private String Name;
+	//Initialize student information 
+	private String studentName;
 	private String Degree;
-	private String Address;
+	private String studentAddress;
     private DB db = DB.getInstance();
     private boolean status;
-    
+    /*
+     * Student constructor 
+     */
 	public Student(String name, String degree,String Address, String ID, String Account, String Password) {
 		super(ID,Account,Password);
-		this.Name = name;
+		this.studentName = name;
 		this.Degree = degree;
-		this.Address = Address;
+		this.studentAddress = Address;
 		status = db.registerStudent(ID, name, Account, Password, Address, degree);
 	}
-	
+	/*
+     * getter method for Student name 
+     */
 	public String getName() {
-		return Name;
+		return studentName;
 	}
-	
+	/*
+     * setter method for Student name 
+     */
 	public void setName(String name) {
-		Name = name;
+		studentName = name;
 	}
-	
+	/*
+     * getter method for student degree
+     */
 	public String getDegree() {
 		return Degree;
 	}
-	
+	/*
+     * setter method for student degree 
+     */
 	public void setDegree(String degree) {
 		Degree = degree;
 	}
-	
+	/*
+     * getter method for Student address 
+     */
 	public String getAddress() {
-		return Address;
+		return studentAddress;
 	}
-
+	/*
+     * setter method for Student address 
+     */
 	public void setAddress(String address) {
-		Address = address;
+		studentAddress = address;
 	}
 	
 	//checking if a stduent is registered in the database or not
