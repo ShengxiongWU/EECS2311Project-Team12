d681df54910c1da84b3511a1036666a14e55cb50
Dave90910 <dave@localhost>
2023-04-09 15:49:02 -0400

    refactoring

--- lib/src/main/java/Backend/Users.java
+++ lib/src/main/java/Backend/Users.java
@@ -3,14 +3,14 @@ package Backend;
 import java.util.ArrayList;
 
 public class Users {
-
+	//Initialize users and user list
 	private ArrayList<User> Users;
 	private static Users User_list;
 	
 	private Users() {
 		Users = new ArrayList<User>();
 	}
-	
+	//get instance of a user
 	 public static Users getInstance() {
 		   if(User_list == null) {
 			   User_list = new Users();
@@ -18,7 +18,7 @@ public class Users {
 		  
 		return User_list;
 	   }
-	 
+	 //Getter method for user
 	 public ArrayList<User> getUsers(){
 		 return Users;
 	 }
