e1dfc1808e24dd1dc82fdb038b38fb28078dc637
Dave90910 <dave@localhost>
2023-04-09 15:40:56 -0400

    refactoring

--- lib/src/main/java/Backend/StubDB.java
+++ lib/src/main/java/Backend/StubDB.java
@@ -12,7 +12,9 @@ public class StubDB {
 		course_list = Courses.getInstance();
 		enroll_list = Enrollments.getInstance();
 	   }
-
+	   /*
+	    * get instance of stubDB
+	    */
 	   public static StubDB getInstance() {
 		   if(db == null) {
 		         db = new StubDB();
@@ -20,17 +22,21 @@ public class StubDB {
 		  
 		return db;
 	   }
-	     
+	/*
+	 * get course list   
+	 */
 	public Courses getCourse_list() {
 		return course_list;
 	}
 
-
+	/*
+	 * get enrollment list   
+	 */
 	public Enrollments getEnroll_list() {
 		return enroll_list;
 	}
 
-
+    //main method 
 	public static void main(String[] args) {
 			StubDB db = StubDB.getInstance();
 			double gpa = db.getEnroll_list().getOverAllGPA("316408091");
